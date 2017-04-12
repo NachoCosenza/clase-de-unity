efe94121c95fcbb87f1f075cf9ab13645e1c2b1b
NachoCosenza <cosenza.lores@hotmail.com>
2017-04-11 21:40:09 -0300

    proyecto basico. primara subida. - esenario basico - 1 diseño de tanque mas controles. - arreglras punta del tanque

---
+++ clase 3/clase 3/Assets/cosa.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cosa : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float angle;
+	public GameObject prefa;
+	public Transform salida;
+	public KeyCode rotacionI;
+	public KeyCode rotacionD;
+	public KeyCode tiro;
+
+
+
+	public int valor = 1 ;
+
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if(Input.GetKey(rotacionD))
+		{
+			transform.Rotate (Vector3.up * Time.deltaTime * angle);
+		}
+
+		if(Input.GetKey(rotacionI))
+		{
+			transform.Rotate (Vector3.up * Time.deltaTime * -angle);
+		}
+		//rb.AddRelativeForce (Vector3.forward);
+		//rb.AddTorque (Vector3.up);
+		//rb.AddRelativeTorque (Vector3.up);
+		if(Input.GetKeyDown(tiro))
+		{
+			Instantiate (prefa,salida.position,salida.rotation);
+		}
+	}
+}
---
+++ clase 3/clase 3/Assets/cosa2.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cosa2 : MonoBehaviour {
+	private Rigidbody rb;
+	public float angle;
+	public KeyCode RotacionAB;
+	public KeyCode RotacionAR;
+	// Use this for initialization
+	void Start () 
+	{
+		rb = GetComponent<Rigidbody>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKey(RotacionAR))
+		{
+			transform.Rotate (Vector3.forward * Time.deltaTime * angle);
+		}
+
+		if(Input.GetKey(RotacionAB))
+		{
+			transform.Rotate (Vector3.forward * Time.deltaTime * -angle);
+		}
+		
+	}
+}
